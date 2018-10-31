2f061bf13bc262546b4970cf13e9f988bf763335
Tom <you@example.com>
2018-10-31 23:35:30 +0000

    added deleting and adding of peeps

--- app/controllers/peep_controller.rb
+++ app/controllers/peep_controller.rb
@@ -10,15 +10,20 @@ class PeepController < ApplicationController
   end
   # posts the new peep to the database
   post "/peeps" do
-
+    unless params[:title].empty?
+      Peep.create(:title => params[:title], :content => params[:content])
+    end
+    # needs to check if either are empty
+    redirect "/peeps"
   end
   # gets a specific peep
   get "/peeps/:id" do
-    erb :'/peeps/'
+    @peep = Peep.find(params[:id])
+    erb :'/peeps/show'
   end
   # gets the edit page for the peep
   get "/peeps/:id/edit" do
-    erb :'/peeps/new'
+    erb :'/peeps/edit'
   end
   # edits the peep with the id :id
   patch '/peeps/:id' do
@@ -26,6 +31,8 @@ class PeepController < ApplicationController
   end
   # deletes the peep id :id
   delete '/peeps/:id/delete' do
-
+    @peep = Peep.find(params[:id])
+    @peep.delete
+    redirect '/peeps'
   end
 end
